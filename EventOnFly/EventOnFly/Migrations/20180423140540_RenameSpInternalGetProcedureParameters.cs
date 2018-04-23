f5a6b754214eb96ebee7101181655a24bde0d10f
marta1994 <mmmz1004@gmail.com>
2018-04-23 17:09:33 +0300

    renamed spInternalGetProcedureParameters

---
+++ EventOnFly/EventOnFly/Migrations/20180423140540_RenameSpInternalGetProcedureParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventOnFly.Migrations
+{
+    public partial class RenameSpInternalGetProcedureParameters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("EXEC sp_rename 'dbo.uspInternalGetProcedureParameters', 'spInternalGetProcedureParameters'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("EXEC sp_rename 'dbo.spInternalGetProcedureParameters', 'uspInternalGetProcedureParameters'");
+        }
+    }
+}
