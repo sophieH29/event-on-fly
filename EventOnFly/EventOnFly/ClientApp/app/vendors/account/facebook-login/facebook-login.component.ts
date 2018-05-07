d106016a71152ffe756624ad27420b26baba067f
sofia.huts <sofia.huts@justanswer.com>
2018-05-07 12:43:47 +0300

    WIP

--- EventOnFly/EventOnFly/ClientApp/app/vendors/account/facebook-login/facebook-login.component.ts
+++ EventOnFly/EventOnFly/ClientApp/app/vendors/account/facebook-login/facebook-login.component.ts
@@ -18,7 +18,7 @@ export class FacebookLoginComponent {
   isRequesting: boolean; 
 
   launchFbLogin() {
-      this.authWindow = window.open('https://www.facebook.com/v2.11/dialog/oauth?&response_type=token&display=popup&client_id=214154915845337&display=popup&redirect_uri=http://localhost:5000/facebook-auth.html&scope=email',null,'width=600,height=400');    
+      this.authWindow = window.open('https://www.facebook.com/v2.11/dialog/oauth?&response_type=token&display=popup&client_id=1528751870549294&display=popup&redirect_uri=http://localhost:5000/facebook-auth.html&scope=email',null,'width=600,height=400');    
   }
 
   constructor(private userService: UserService, private router: Router) {
